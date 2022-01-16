32d3a151988bae05f6f3bcac55e105b736ffab46
Ricky Thramkrongart <admin@thram.dk>
2022-01-16 14:53:57 +0100

    Added Regexp

--- TestTapeGenerator/TestTapeGenerator.ino
+++ TestTapeGenerator/TestTapeGenerator.ino
@@ -12,6 +12,7 @@
 #include "Controls.h"
 #include "Printer.h"
 #include <DS3232RTC.h>      // https://github.com/JChristensen/DS3232RTC
+#include <Regexp.h>         // https://github.com/nickgammon/Regexp
 using namespace std;
 
 double randomDouble(double minf, double maxf)
@@ -253,40 +254,60 @@ void SetDateTime()
 {
     LCD_Helper lcdhelper;
     RTC_Helper rtchelper;
-    lcdhelper.line[0] = "Set DateTime";
+    lcdhelper.line[0] = "Reading Date Time from Serial/COM port";
+    lcdhelper.line[1] = "Format: [yyyy/mm/dd HH.MM.SS]";
     lcdhelper.Show();
-    Serial.begin(115200); // opens serial port, sets data rate to 9600 bps
     Serial.setTimeout(500);
+    while (Serial.available() > 0) Serial.read();
+    Serial.flush();
+    Serial.println(lcdhelper.line[0].c_str());
+    Serial.println(lcdhelper.line[1].c_str());
 
     do {
-        if (Serial.available() >= 12) {
-            time_t t;
-            tmElements_t tm;
-            tm.Year = CalendarYrToTm(Serial.parseInt());
-            tm.Month = Serial.parseInt();
-            tm.Day = Serial.parseInt();
-            tm.Hour = Serial.parseInt();
-            tm.Minute = Serial.parseInt();
-            tm.Second = Serial.parseInt();
-            t = makeTime(tm);
-            RTC.set(t);        // use the time_t value to ensure correct weekday is set
-            setSyncProvider(RTC.get);   // the function to get the time from the RTC
-            char sprintfbuffer[256];
-            sprintf(sprintfbuffer, "%04i:%02i:%02i %02i:%02i:%02i", year(), month(), day(), hour(), minute(), second());
-            lcdhelper.line[3] = sprintfbuffer;
-            lcdhelper.Show();
+        if (Serial.available() >= 19) {
+            MatchState ms;
+            String str(Serial.readString());
+            ms.Target(str.c_str());
+            char result = ms.Match ("(%d%d%d%d)/(%d%d)/(%d%d) (%d%d)\.(%d%d)\.(%d%d)");
+            char cap[256];
+            if (result == REGEXP_MATCHED)
+            {
+                time_t t;
+                tmElements_t tm;
+                int index = 0;
+                ms.GetCapture (cap, index++);
+                uint16_t y = atoi(cap);
+                tm.Year = CalendarYrToTm(atoi(cap));
+                ms.GetCapture (cap, index++);
+                tm.Month = atoi(cap);
+                ms.GetCapture (cap, index++);
+                tm.Day = atoi(cap);
+                ms.GetCapture (cap, index++);
+                tm.Hour = atoi(cap);
+                ms.GetCapture (cap, index++);
+                tm.Minute = atoi(cap);
+                ms.GetCapture (cap, index++);
+                tm.Second = atoi(cap);
+                t = makeTime(tm);
+                RTC.set(t);        // use the time_t value to ensure correct weekday is set
+                setSyncProvider(RTC.get);   // the function to get the time from the RTC
+                lcdhelper.line[2] = "Recieved Date Time.";
+                lcdhelper.Show();
+                Serial.println(lcdhelper.line[2].c_str());
+                delay(2000);
+                return;
+            }
+            while (Serial.available() > 0) Serial.read();
         }
     } while (true);
-
 }
-
-
 
 void setup()
 {
+    Serial.begin(115200);
     splashscreen();
     selftest();
-  
+
     do {
         MainMenu mainMenu;
         if (mainMenu.Execute()) {
