9f927ef3b37c9ff78c3ef6d0251cf72a05a923ab
Ricky Thramkrongart <admin@thram.dk>
2022-01-17 11:26:49 +0100

    Added SignalGenerator

--- TestTapeGenerator/TestTapeGenerator.ino
+++ TestTapeGenerator/TestTapeGenerator.ino
@@ -13,8 +13,60 @@
 #include "Controls.h"
 #include "Printer.h"
 #include <DS3232RTC.h>      // https://github.com/JChristensen/DS3232RTC
-#include <Regexp.h>         // https://github.com/nickgammon/Regexp
+#include <Regexp.h>         // https://github.com/nickgammon/Regexp/
 using namespace std;
+
+class SignalGenerator
+{
+  protected:
+        // for chip info see https://www.analog.com/en/products/ad9833.html
+        // SPI code taken from https://github.com/MajicDesigns/MD_AD9833/
+        const uint8_t _clkPin;
+        const uint8_t _fsyncPin;
+        const uint8_t _dataPin;
+    public:
+        SignalGenerator(): _clkPin(13), _fsyncPin(2), _dataPin(9)
+        {
+            pinMode(_clkPin,   OUTPUT);
+            pinMode(_fsyncPin, OUTPUT);
+            pinMode(_dataPin,  OUTPUT);
+            digitalWrite(_fsyncPin, HIGH);
+            digitalWrite(_clkPin,   LOW);
+        }
+
+        void spiSend(const uint16_t data)
+        {
+            digitalWrite(_fsyncPin, LOW);
+
+            uint16_t m = 1UL << 15;
+            for (uint8_t i = 0; i < 16; i++)
+            {
+                digitalWrite(_dataPin, data & m ? HIGH : LOW);
+                digitalWrite(_clkPin, LOW); //data is valid on falling edge
+                digitalWrite(_clkPin, HIGH);
+                m >>= 1;
+            }
+            digitalWrite(_dataPin, LOW); //idle low
+            digitalWrite(_fsyncPin, HIGH);
+        }
+
+        void setFreq(double f)
+        {
+            const uint16_t b28  = (1UL << 13);
+            const uint16_t freq = (1UL << 14);
+
+            const double   f_clk = 25e6;
+            const double   scale = 1UL << 28;
+            const uint32_t n_reg = f * scale / f_clk;
+
+            const uint16_t f_low  = n_reg         & 0x3fffUL;
+            const uint16_t f_high = (n_reg >> 14) & 0x3fffUL;
+
+            spiSend(b28);
+            spiSend(f_low  | freq);
+            spiSend(f_high | freq);
+        }
+};
 
 double randomDouble(double minf, double maxf)
 {
@@ -256,7 +308,7 @@ void SetDateTime()
     LCD_Helper lcdhelper;
     RTC_Helper rtchelper;
     lcdhelper.line[0] = "Reading Date Time from Serial/COM port";
-    lcdhelper.line[1] = "Format: [yyyy/mm/dd HH.MM.SS]";
+    lcdhelper.line[1] = "Format: [yyyy/mm/dd HH.MM.SS] @ (115200)";
     lcdhelper.Show();
     Serial.setTimeout(500);
     while (Serial.available() > 0) Serial.read();
@@ -306,6 +358,9 @@ void SetDateTime()
 void setup()
 {
     Serial.begin(115200);
+    SignalGenerator signalGenerator;
+    signalGenerator.setFreq(1000);
+        
     splashscreen();
     selftest();
 
