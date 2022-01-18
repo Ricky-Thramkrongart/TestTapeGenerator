208bee58b3c8871a4cf16285cb59178ee2abb568
Ricky Thramkrongart <ricky.thramkrongart@man-es.com>
2022-01-18 14:29:54 +0100

    Added Fit

--- TestTapeGenerator/MakeTable.ps1
+++ TestTapeGenerator/MakeTable.ps1
@@ -11,6 +11,14 @@ function Create-Table
 	
 	[string]$HeaderBlock = "$($CSVFile.basename.ToUpper())_H"
 	
+
+	[System.Globalization.CultureInfo]$saveCurrentCulture = [System.Threading.Thread]::CurrentThread.CurrentCulture
+	[System.Globalization.CultureInfo]$saveCurrentUICulture = [System.Threading.Thread]::CurrentThread.CurrentUICulture
+		
+	[System.Threading.Thread]::CurrentThread.CurrentCulture = [System.Globalization.CultureInfo]::new('en-DK')
+	[System.Threading.Thread]::CurrentThread.CurrentUICulture = [System.Globalization.CultureInfo]::new('en-US')
+
+
 	[array]$Header = @('A00')
 	$OutPutTable = import-csv $CSVFile -Delimiter ';' -Header $Header
 	[array]$TableDouble
@@ -25,11 +33,9 @@ function Create-Table
 		}
 		$TableDouble += ,$array
 	}
-	[System.Globalization.CultureInfo]$saveCurrentCulture = [System.Threading.Thread]::CurrentThread.CurrentCulture
-	[System.Globalization.CultureInfo]$saveCurrentUICulture = [System.Threading.Thread]::CurrentThread.CurrentUICulture
 	
-	[System.Threading.Thread]::CurrentThread.CurrentCulture = [System.Globalization.CultureInfo]::new(1033)
-	[System.Threading.Thread]::CurrentThread.CurrentUICulture = [System.Globalization.CultureInfo]::new(1033)
+	[System.Threading.Thread]::CurrentThread.CurrentCulture = [System.Globalization.CultureInfo]::new('en-US')
+	[System.Threading.Thread]::CurrentThread.CurrentUICulture = [System.Globalization.CultureInfo]::new('en-US')
 	
 	[array]$row = @()
 	for ([int]$i = 0; $i -ne $TableDouble.Length; $i++)
@@ -41,6 +47,14 @@ function Create-Table
 		"#define $HeaderBlock",
 		"",
 		"const PROGMEM double $($CSVFile.basename)[$($OutPutTable.Length)] = $($row -join ', ');",
+		"",
+		"uint8_t $($CSVFile.basename)Fit (double dB)",
+		"{",
+		"     uint8_t rv = 0.00001020475 * pow(dB,6) - 0.000634453 * pow(dB, 5) + 0.012749614 * pow(dB, 4) - 0.058752793 * pow(dB, 3) - 0.352027967 * pow(dB, 2) - 16.60582415 * dB + 254.8871207;",
+		"     rv = std::min(rv, (uint8_t)256);",
+		"     rv = std::max(rv, (uint8_t)0);",
+		"     return rv;",
+		"}",
 		"",
 		"#endif // $HeaderBlock"
 	) | Set-Content -Path ".\$($CSVFile.basename).h"
